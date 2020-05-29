8f03660b19b5724564a6ff59f7ad62713743820b
Mehboob750 <ms568203@gmail.com>
2020-05-29 09:08:50 +0530

    Welcome to Mood Analyser Problem

---
+++ ConsoleApplicationMoodAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplicationMoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
